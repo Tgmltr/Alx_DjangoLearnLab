5d7bbaa65f910628b14dc0cd0fabe81c65f054eb
ETaguemalter <eh.taguemalter@gmail.com>
2024-11-03 18:48:50 +0100

    updated admin file

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,5 @@ from django.contrib import admin
 from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year',)
 admin.site.register(Book, BookAdmin)
-
-
